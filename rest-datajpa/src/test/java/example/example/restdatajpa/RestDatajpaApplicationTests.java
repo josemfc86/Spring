a60cc29934b5f6372d9fe1ede5dba8031d4c90f7
josemfernandez <josemfc86@gmail.com>
2023-10-19 18:24:26 +0200

    Se agrega proyecto

---
+++ rest-datajpa/src/test/java/example/example/restdatajpa/RestDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package example.example.restdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
